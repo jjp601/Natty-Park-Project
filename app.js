f759d4218921f6931ac9dd14d66f9519ffb04524
Jon Powell <jpowell68601@gmail.com>
2018-04-10 23:30:12 -0400

    Update Environment variables

--- app.js
+++ app.js
@@ -19,8 +19,9 @@ var commentRoutes   = require("./routes/comments"),
     indexRoutes     = require("./routes/index");
 
 
-// mongoose.connect("mongodb://localhost/natty_park");
-mongoose.connect("mongodb://jpowell1:Concordia93@ds241019.mlab.com:41019/nattypark");
+var databaseURL = process.env.DATABASE || "mongodb://localhost/natty_park";
+mongoose.connect(databaseURL);
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
